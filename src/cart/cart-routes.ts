3b92ab0c93fb535a0012f90a16fcea04af5eef06
juan <barchettajuan.barche@gmail.com>
2025-11-06 15:48:41 -0300

    arreglo de cart-routes

--- src/cart/cart-routes.ts
+++ src/cart/cart-routes.ts
@@ -1,4 +1,4 @@
-import { Router } from "express";
+import { RequestHandler, Router } from "express";
 import { CartController } from './cart-controller.js';
 
 
@@ -6,13 +6,19 @@ export class CartRouter {
     public readonly router = Router();
     constructor(){
         const controller = new CartController();
-        this.router.post('/add', controller.addToCart);
-        this.router.post('/remove', controller.removeFromCart);
-        this.router.get('/products', controller.getCartProducts);
-        this.router.get('/total', controller.getCartTotal);
-        this.router.post('/confirm', (req, res) => controller.confirmCart(req, res));
+
+        const add: RequestHandler = (req, res) => { void controller.addToCart(req, res); };
+        const remove: RequestHandler = (req, res) => { void controller.removeFromCart(req, res); };
+        const products: RequestHandler = (req, res) => { void controller.getCartProducts(req, res); };
+        const total: RequestHandler = (req, res) => { void controller.getCartTotal(req, res); };
+        const confirm: RequestHandler = (req, res) => { void controller.confirmCart(req, res); };
+
+        this.router.post('/add', add);
+        this.router.post('/remove', remove);
+        this.router.get('/products', products);
+        this.router.get('/total', total);
+        this.router.post('/confirm', confirm);
     }
 }
-
 
 
